ea34050b35049d7017dbe614757963752439b4c3
Jane Philipps <jane.philipps@gmail.com>
2015-05-10 18:49:33 -0700

    Update User model for Devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable, :registerable, :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable
 end
