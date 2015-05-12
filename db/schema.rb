1f1267c0e63ce94ff2856d8851f694738024fbd0
Jane Philipps <jane.philipps@gmail.com>
2015-05-11 20:28:16 -0700

    Change column names in Users table, add custom Devise fields

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150511191758) do
+ActiveRecord::Schema.define(version: 20150512031932) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,7 +25,7 @@ ActiveRecord::Schema.define(version: 20150511191758) do
     t.datetime "updated_at",    null: false
   end
 
-  create_table "establishments", force: :cascade do |t|
+  create_table "destinations", force: :cascade do |t|
     t.string   "name"
     t.string   "location"
     t.string   "website"
@@ -47,8 +47,8 @@ ActiveRecord::Schema.define(version: 20150511191758) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "firstName"
-    t.string   "lastName"
+    t.string   "first_name"
+    t.string   "last_name"
     t.datetime "created_at",                      null: false
     t.datetime "updated_at",                      null: false
     t.string   "email",              default: "", null: false
