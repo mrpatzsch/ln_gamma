fdb059651acd7f0fa2b5c5b673de8ef54d8028a2
Jane Philipps <jane.philipps@gmail.com>
2015-05-12 17:02:55 -0700

    Update bucketlist policy

--- app/policies/bucketlist_policy.rb
+++ app/policies/bucketlist_policy.rb
@@ -1,5 +1,22 @@
 class BucketlistPolicy < ApplicationPolicy
   attr_reader :user, :list_item
 
+  def initialize(user, list_item)
+    @user = user
+    @list_item = list_item
+  end
+
+  def create?
+    user_signed_in?
+  end
+
+  def update?
+    user_signed_in?
+  end
+
+  def destroy?
+    user_signed_in?
+  end
+
 
 end
