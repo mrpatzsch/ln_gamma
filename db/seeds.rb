8591c8a29917d9e9ed500b6e6a50e0d5606c9970
Arati Sureddi <aps913@Aratis-MacBook-Pro-2.local>
2015-05-12 17:25:39 -0700

    db destination & cert hotels seed & nav partials

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,199 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+certified_hotels = Certified_hotels.create([
+    { name: 'University of Maryland Inn & Conference',
+    hotel_id: '',
+    certification: 'LEED',}
+    { name: 'GAIA Napa Valley',
+    hotel_id: 'Len Foote Hike Inn',
+    certification: 'LEED',}
+    { name: 'Hilton Vancouver Conference Center & Hotel, Vancouver WASHINGTON',
+    hotel_id: '227029',
+    certification: 'LEED',}
+    { name: 'Gaia Shasta',
+    hotel_id: '',
+    certification: 'LEED',}
+    { name: 'Suwanee River Eco Lodge',
+    hotel_id: '',
+    certification: 'LEED',}
+    { name: 'The Lodge & Spa at Callaway Gardens',
+    hotel_id: '',
+    certification: 'LEED',}
+    { name: 'W Hollywood Hotel & Residences',
+    hotel_id: '335297',
+    certification: 'LEED',}
+    { name: 'Proximity Hotel',
+    hotel_id: '',
+    certification: 'LEED',}
+    { name: 'Orchard Garden Hotel - San Francisco',
+    hotel_id: '248276',
+    certification: 'LEED',}
+    { name: 'Northstar Village ',
+    hotel_id: '',
+    certification: 'LEED',}
+    { name: 'Unity Village Hotel & Conference Center',
+    hotel_id: '',
+    certification: 'LEED',}
+    { name: 'The Nines Hotel',
+    hotel_id: '',
+    certification: 'LEED',}
+    { name: 'Aria Hotel Tower',
+    hotel_id: '',
+    certification: 'LEED',}
+    { name: 'Sandpearl Resort & Hotel',
+    hotel_id: '',
+    certification: 'LEED',}
+    { name: 'Westin Riverfront Resort & Spa',
+    hotel_id: '265697',
+    certification: 'LEED',}
+    { name: 'Ritz Carlton Charlotte, Bank Of America',
+    hotel_id: '',
+    certification: 'LEED',}
+    { name: 'Hotel Terra',
+    hotel_id: '',
+    certification: 'LEED',}
+    { name: 'Three PNC Plaza',
+    hotel_id: '',
+    certification: 'LEED',}
+    { name: 'YMCA - Greenkill Retreat Center',
+    hotel_id: '',
+    certification: 'LEED',}
+    { name: 'Village North Condominiums',
+    hotel_id: '',
+    certification: 'LEED',}
+    { name: 'Honey Creek Resort State Park',
+    hotel_id: '',
+    certification: 'LEED',}
+    { name: 'Bardessono',
+    hotel_id: '',
+    certification: 'LEED',}
+    { name: 'Hotel: Posada de Mike Rapu',
+    hotel_id: '',
+    certification: 'LEED',}
+    { name: 'Orchard Hotel - San Francisco',
+    hotel_id: '192358',
+    certification: 'LEED',}
+    { name: 'Drury Inn & Suites Flagstaff',
+    hotel_id: '',
+    certification: 'LEED',}
+    { name: 'The Palazzo',
+    hotel_id: '',
+    certification: 'LEED',}
+    { name: 'LA Convention Center Hotel and Residence',
+    hotel_id: '',
+    certification: 'LEED',}
+    { name: 'CityFlats Hotel - Charter House Holdings',
+    hotel_id: '',
+    certification: 'LEED',}
+    { name: 'Cavallo Point Lodge',
+    hotel_id: '265998',
+    certification: 'LEED',}
+    { name: 'Hotel Andaluz (La Posada Hotel)',
+    hotel_id: '',
+    certification: 'LEED',}
+    { name: 'Hostal Empuries',
+    hotel_id: '',
+    certification: 'LEED',}
+    { name: 'Aloft Hotel- Tempe Arizona',
+    hotel_id: '',
+    certification: 'LEED',}
+    { name: 'The Ambrose',
+    hotel_id: '',
+    certification: 'LEED',}
+    { name: 'Vdara Condo Hotel (CityCenter)',
+    hotel_id: '',
+    certification: 'LEED',}
+    { name: 'Miyako Hybrid Hotel',
+    hotel_id: '',
+    certification: 'LEED',}
+    { name: 'Hotel Arista',
+    hotel_id: '',
+    certification: 'LEED',}
+    { name: 'Crosby Street Hotel',
+    hotel_id: '',
+    certification: 'LEED',}
+    { name: 'The Springs Hotel',
+    hotel_id: '',
+    certification: 'LEED',}
+    { name: 'Starwood Element - Lexington',
+    hotel_id: '',
+    certification: 'LEED',}
+    { name: 'Sheraton Incheon Hotel',
+    hotel_id: '',
+    certification: 'LEED',}
+    { name: 'Courtyard by Marriott Portland City Center',
+    hotel_id: '',
+    certification: 'LEED',}
+    { name: 'Hotel Indigo, San Diego',
+    hotel_id: '',
+    certification: 'LEED',}
+    { name: 'George Mason University Hotel & Conference Center',
+    hotel_id: '',
+    certification: 'LEED',}
+    { name: 'Boone Tavern',
+    hotel_id: '',
+    certification: 'LEED',}
+    { name: 'Fairmont Pittsburgh',
+    hotel_id: '',
+    certification: 'LEED',}
+    { name: 'Avalon Hotel and Spa',
+    hotel_id: '',
+    certification: 'LEED',}
+    { name: 'The Allison Inn & Spa',
+    hotel_id: 'Montage Hotel Beverly Hills',
+    certification: 'LEED',}
+    { name: 'Hyatt Place Grand Rapids - South',
+    hotel_id: '',
+    certification: 'LEED',}
+    { name: 'DoubleTree Executive Meeting Center, Palm Beach',
+    hotel_id: '',
+    certification: 'LEED',}
+    { name: 'Fairfield Inn and Suites- Baltimore',
+    hotel_id: '',
+    certification: 'LEED',}
+    { name: 'The Johnson Foundation Guest House - Racine, WI',
+    hotel_id: '',
+    certification: 'LEED',}
+    { name: 'Courtyard, Chevy Chase, MD',
+    hotel_id: '',
+    certification: 'LEED',}
+    { name: 'Wyndham Vacation Ownership, Orlando, FL',
+    hotel_id: '',
+    certification: 'LEED',}
+    { name: 'Hotel Hershey, Hershey PA',
+    hotel_id: '',
+    certification: 'LEED',}
+    { name: 'Kimpton Palomar-Philadelphia',
+    hotel_id: '',
+    certification: 'LEED',}
+    { name: 'Element Houston',
+    hotel_id: '',
+    certification: 'LEED',}
+    { name: 'Hotel Indigo at the Rialto Center',
+    hotel_id: '',
+    certification: 'LEED',}
+    { name: 'Venetian Resort, Las Vegas',
+    hotel_id: '',
+    certification: 'LEED',}
+    { name: 'Hampton Inn and Suites Sarasota Airport',
+    hotel_id: '',
+    certification: 'LEED',}
+    { name: 'Mandarin Oriental, Las Vegas',
+    hotel_id: '',
+    certification: 'LEED',}
+    { name: 'Hotel Carlton, San Francisco, CA',
+    hotel_id: '',
+    certification: 'LEED',}
+    { name: 'J.W. Marriott San Antonio Resort',
+    hotel_id: '',
+    certification: 'LEED',}
+
+
+
+
+    }])
+
 destinations = Destination.create([
   { name: 'Akha Ama',
     location: 'Chiang Mai, Thailand',
