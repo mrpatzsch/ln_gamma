c077fec7cee76f7b938f5426fc6d2a8fc4ceabbe
Jane Philipps <jane.philipps@gmail.com>
2015-05-13 18:29:40 -0700

    Commented out paperclip stuff

--- app/models/destination.rb
+++ app/models/destination.rb
@@ -4,7 +4,7 @@ class Destination < ActiveRecord::Base
   accepts_nested_attributes_for :bucketlist_items
 
 
-  has_attached_file :image, styles: { large: "670x520", medium: "300x220>", thumb: "150x150#" }
-  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+  # has_attached_file :image, styles: { large: "670x520", medium: "300x220>", thumb: "150x150#" }
+  # validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 
 end
