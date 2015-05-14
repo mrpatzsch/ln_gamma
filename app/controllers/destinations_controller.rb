b80a627629904db195d6902e9e0da36b72c81981
Andrew Patzsch <mrpatzsch@gmail.com>
2015-05-13 20:31:56 -0700

    fix bugs and dead links

--- app/controllers/destinations_controller.rb
+++ app/controllers/destinations_controller.rb
@@ -12,4 +12,17 @@ class DestinationsController < ApplicationController
 		@destination = Destination.find(params[:id])
 	end
 
+	def update
+    destination_id = params[:id]
+    destination = Destination.find(destination_id)
+
+    destination.update_attributes(destination_params)
+
+    redirect_to "/destinations/#{destination.id}"
+  end
+
+private
+  def destination_params
+      params.require(:destination).permit(:name, :location, :website, :certification, :category, :desc_prod, :desc_good, :directions, :recommended, :seo_tags, :image )
+    end
 end
