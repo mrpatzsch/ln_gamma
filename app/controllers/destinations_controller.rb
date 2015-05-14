4e7068d7295ab27f6e66084272a453ffae0c1361
Arati Sureddi <aps913@Aratis-MacBook-Pro-2.local>
2015-05-13 19:20:26 -0700

    destinations controller, form & edit view

--- app/controllers/destinations_controller.rb
+++ app/controllers/destinations_controller.rb
@@ -4,8 +4,29 @@ class DestinationsController < ApplicationController
 	end
 	
 	def show
-		@destinations = Destination.find(params[:id])
+		@destination = Destination.find(params[:id])
 	end
 
-	
+	def edit
+		@destination = Destination.find(params[:id])
+	end
+
+	def update
+    respond_to do |format|
+      if @destination.update(destination_params)
+        format.html { redirect_to @destination, notice: 'Destination was successfully updated.' }
+        format.json { render :show, status: :ok, location: @destination }
+      else
+        format.html { render :edit }
+        format.json { render json: @destination.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+private
+  def destination_params
+      params.require(:destination).permit(:name, :location, :website, :certification, :category, :desc_prod, :desc_good, :directions, :recommended, :seo_tags, :image )
+    end
+
+
 end
