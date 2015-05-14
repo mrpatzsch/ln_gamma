17932c26ada997e97ed6a41e20d57cfb5a54b5b9
Andrew Patzsch <mrpatzsch@gmail.com>
2015-05-14 11:31:19 -0700

    Hid API keys

--- app/models/hotel.rb
+++ app/models/hotel.rb
@@ -7,7 +7,7 @@ class Hotel < ActiveRecord::Base
     fixed_depart = depart.gsub(/(\d{4})-(\d{2})-(\d{2})/, '\2/\3/\1')
     fixed_destination = destination.try(:split,(" "))
     destination_fixed = fixed_destination.try(:join,("+"))
-    response = Typhoeus.get("http://api.ean.com/ean-services/rs/hotel/v3/list?geoSearch?minorRev=5&apiKey=6bjgco74rs2j90o5mkhmsabuqe&sig=6bjgco74rs2j90o5mkhmsabuqe8fj76j23ppr001431025260622&cid=55505&locale=en_US&destinationString=#{destination_fixed}&numberOfResults=40&arrivalDate=#{fixed_arrive}&departureDate=#{fixed_depart}&sort=DEFAULT&_type=json")
+    response = Typhoeus.get("http://api.ean.com/ean-services/rs/hotel/v3/list?geoSearch?minorRev=5&apiKey=" + ENV['EAN_key'] + "&cid=55505&locale=en_US&destinationString=#{destination_fixed}&numberOfResults=40&arrivalDate=#{fixed_arrive}&departureDate=#{fixed_depart}&sort=DEFAULT&_type=json")
     
     hotels_data = JSON.parse(response.body)
 
@@ -15,7 +15,7 @@ class Hotel < ActiveRecord::Base
   end
 
   def self.show_hotel(hotelId)
-    response = Typhoeus.get("http://api.ean.com/ean-services/rs/hotel/v3/info?cid=55505&minorRev=99&apiKey=6bjgco74rs2j90o5mkhmsabuqe&locale=en_US&currencyCode=USD&_type=JSON&hotelId=#{hotelId}")
+    response = Typhoeus.get("http://api.ean.com/ean-services/rs/hotel/v3/info?cid=55505&minorRev=99&apiKey=" + ENV["EAN_key"] + "&locale=en_US&currencyCode=USD&_type=JSON&hotelId=#{hotelId}")
     
     hotel_data = JSON.parse(response.body)
 
