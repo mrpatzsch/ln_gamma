3c851213228bd6543959c8c264cbede3e08208f6
Andrew Patzsch <mrpatzsch@gmail.com>
2015-05-14 20:41:15 -0700

    try to include db filter

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -10,6 +10,26 @@ class SiteController < ApplicationController
         hotels = Hotel.show_hotels(destination, arrive, depart)
         @hotels = hotels["HotelListResponse"]["HotelList"]["HotelSummary"]
 
+      hotelids = []
+      certified = []
+      new_array = []
+      @hotels.each do |hotel|
+         hotelids.push(hotel["hotelId"])
+      end
+      puts "\n\n\n\n", hotelids
+      @certified_hotels = CertifiedHotel.all
+      hotelids.concat certified
+      new_array.concat  hotelids
+
+      @certified_hotels.each do |certify|
+        certified.push(certify.hotel_id)
+      end
+      puts "\n\n\n\n\n certified", certified
+
+
+    puts "\n\n\n\n", new_array.length
+    final_array = new_array.select{|item| new_array.count(item) > 1}
+    puts "\n\n\n\n\n\n\n\n\n Certified Hotels", final_array
       # Iterate through API response and compare to Certified hotel DB 
       # render results 
     end
